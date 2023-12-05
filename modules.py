2dc61c6bcb789c3fb70fd63bb5b226eb34d2b4de
NIkita VAsilchenko <nikita@mail.rur>
2023-12-05 19:26:24 +0300

    COrn

--- modules.py
+++ modules.py
@@ -1,1 +1,3 @@
-# A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+
+
+#CORN WANT MY SON  A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
